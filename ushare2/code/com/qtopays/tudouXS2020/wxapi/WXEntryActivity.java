805e9cb4745f17152e6bfc039444378a7007500c
songjian <songjianzaina@163.com>
2020-04-20 20:41:03 +0800

    1.0.0 集成友盟第三方登录 移除第三方登录库

---
+++ ushare2/code/com/qtopays/tudouXS2020/wxapi/WXEntryActivity.java
@@ -0,0 +1,18 @@
+package com.qtopays.tudouXS2020.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * @ProjectName: cloudpay_new
+ * @Package: com.insworks.ushare.wxapi
+ * @ClassName: WXEntryActivity
+ * @Author: Song Jian
+ * @CreateDate: 2019/8/28 10:40
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/8/28 10:40
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Description: 微信登录需要该类 不可缺 否则登录回调无响应
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+}
